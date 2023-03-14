2d475eddc0ecf1067da6bec92aacb7c79cabb6ce
Daniele Rapisarda <danielerapisarda@danieles-mbp.speedport.ip>
2023-03-14 13:59:25 +0100

    first commit 🎉

---
+++ components/List.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1>
+      add or remove
+    </h1>
+    <ul>
+      <li v-for="index in items" @click="() => addItem(index)"> {{ index }}</li>
+    </ul>
+    <p v-for="(value, index) in itemsList" @click="() => removeItem(index)">
+      {{ value }}
+    </p>
+
+  </div>
+</template>
+  
+  
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        'lol',
+        'lal',
+        'lel'
+      ],
+      itemsList: [],
+    }
+  },
+  methods: {
+    addItem(item) {
+      this.itemsList.push(item);
+    },
+    removeItem(index) {
+      this.itemsList.splice(index, 1)
+    }
+  }
+}
+</script>
