8c4d831ac149a456fbde931a4d502ce7b0f6a183
Daniele Rapisarda <danielerapisarda@danieles-mbp.speedport.ip>
2023-05-19 11:43:01 +0200

    description and styles

--- components/List.vue
+++ components/List.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="steps-list section">
     <div class="container">
-      <div class="steps-list__list steps-list__list--choices block">
+      <div class="steps-list__list steps-list__list--routine block">
         <ul>
-          <li class="card" :class="{ 'mb-4': index !== steps.length - 1 }" v-for="(value, index) in steps" :key="index">
+          <li class="card" :class="{ 'mb-4': index !== steps.length - 1 }" v-for="(step, index) in steps" :key="index">
             <header class="card-header">
               <p class="card-header-title">
-                {{ value.name }} - {{ value.duration / 1000 }}s
+                {{ step.name }} - {{ step.duration / 1000 }}s
               </p>
               <button @click="() => moveStep(index, -1)" class="card-header-icon"
                 :class="{ 'card-header-icon--inactive': index === 0 }" aria-label="move up">
@@ -16,8 +16,8 @@
                 :class="{ 'card-header-icon--inactive': index === steps.length - 1 }" aria-label="move down">
                 ⬇️
               </button>
-              <button class="card-header-icon" aria-label="remove element" @click="() => removeStep(value.name, index)">
-                ➖
+              <button class="card-header-icon" aria-label="remove element" @click="() => removeStep(step.name, index)">
+                🗑
               </button>
             </header>
           </li>
@@ -28,23 +28,34 @@
           <span>➕</span>
         </button>
       </div>
-      <div class="modal" :class="{ 'is-active': modalIsOpen }">
+      <div class="modal section" :class="{ 'is-active': modalIsOpen }">
         <div class="modal-background" @click="toggleModal"></div>
         <div class="modal-content">
           <div class="steps-list__list steps-list__list--options block">
+            <h3 class="block is-size-3"> Add steps to your routine </h3>
+            <p class="block">
             <ul>
-              <li class="card" :class="{ 'mb-4': index !== stepsOptions.length - 1 }"
-                v-for="(value, index) in stepsOptions" :key="index" @click="() => addStep(value)">
-                <header class="card-header">
-                  <p class="card-header-title">
-                    {{ value.name }} - {{ value.duration / 1000 }}s
-                  </p>
-                  <button class="card-header-icon" aria-label="add element">
-                    ➕
-                  </button>
-                </header>
+              <li v-for="(step, index) in stepsOptions" :key="index">
+                <button class="card" :class="{ 'mb-4': index !== stepsOptions.length - 1 }" @click="() => addStep(step)">
+                  <header class="card-header">
+                    <p class="card-header-title">
+                      {{ step.name }} - {{ step.duration / 1000 }}s
+                    </p>
+                    <button class="card-header-icon" aria-label="add element">
+                      ➕
+                    </button>
+                  </header>
+                  <div class="card-content">
+                    <div class="content">
+                      <p>
+                        {{ step.description }}
+                      </p>
+                    </div>
+                  </div>
+                </button>
               </li>
             </ul>
+            </p>
           </div>
         </div>
       </div>
@@ -64,7 +75,7 @@ export default defineComponent({
     const toggleModal = () => {
       modalIsOpen.value = !modalIsOpen.value;
     };
-    const {setToastMessage} = useGlobalStore();
+    const { setToastMessage } = useGlobalStore();
 
     const { steps, stepsOptions, addStepAtTheBottom, removeStep: removeStepStore, moveStep } =
       useRoutineStore();
@@ -97,8 +108,27 @@ export default defineComponent({
 
 .steps-list {
   ul {
-    flex: 1;
     list-style: none;
+  }
+
+  &__list--options {
+    button.card {
+      text-align: left;
+      padding: 0;
+
+      cursor: pointer;
+      width: 100%;
+    }
+
+    .card-header-icon {
+      flex: 0;
+    }
+
+    ul {
+      display: grid;
+      column-gap: $size-4;
+      grid-template-columns: repeat( auto-fit, minmax(250px, 1fr) );
+    }
   }
 
   .card-header-icon--inactive {
