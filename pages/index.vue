64fd64375c64fd74383786db353df76fc7c6eb56
Daniele Rapisarda <danielerapisarda@danieles-mbp.speedport.ip>
2023-05-22 14:20:25 +0200

    hero

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,21 @@
 <template>
   <section class="hero hero--home">
-    <div class="hero-body">
-      <p class="title">
-        Welcome to CalmRoots
-      </p>
-      <p class="subtitle">
-        Create your custom meditation routine. Ready when you are. Everywhere you are.
-      </p>
-      <div class="hero-button">
-        <a href="#routine" class="button is-success">
-          Create your routine now
-        </a>
+    <div class="hero-body columns is-8 is-variable">
+      <div class="column hero-bg"></div>
+      <div class="column hero-claim">
+        <p class="title block is-spaced">
+          Your custom meditation routine. <br>
+          Ready when you are. <br>
+          Everywhere you are.
+        </p>
+        <p class="subtitle block">
+          Build a routine, enjoy presence.
+        </p>
+        <div class="hero-button block">
+          <a href="#routine" class="button is-success">
+            Create your routine now
+          </a>
+        </div>
       </div>
     </div>
   </section>
@@ -23,16 +28,22 @@
 
 <style lang="scss" scoped>
 @import "@/style/vars.scss";
+
 .hero {
-  background: url('/assets/img/forest_bg.webp') no-repeat top center $bg;
-  background-size: cover;
   min-height: 100svh;
+
   .title {
     color: $clear;
+    font-size: clamp(100%, 1rem + 2vw, $size-1);
   }
-  
+
+  .hero-bg {
+    background: url('/assets/img/forest_bg.webp') no-repeat top center $bg;
+    background-size: cover;
+  }
+
   .subtitle {
-    color: $clear;
+    color: $clear-1;
   }
 }
 </style>
