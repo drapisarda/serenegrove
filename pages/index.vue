f71b309ca4bbc3cb84585f5f9427fc72587618a0
Daniele Rapisarda <danielerapisarda@danieles-mbp.speedport.ip>
2023-05-22 17:19:12 +0200

    ui

--- pages/index.vue
+++ pages/index.vue
@@ -3,21 +3,28 @@
     <div class="hero-body columns is-8 is-variable">
       <div class="column hero-bg"></div>
       <div class="column hero-claim">
-        <p class="title block is-spaced">
-          Your custom meditation routine. <br>
-          Ready when you are. <br>
-          Everywhere you are.
-        </p>
-        <p class="subtitle block">
+        <div class="block">
+          <h1 class="title is-spaced is-2">
+            Your custom meditation routine.
+          </h1>
+          <h2 class="title is-4">
+            Ready when you are. <br>
+            Everywhere you are.
+          </h2>
+        </div>
+        <p class="subtitle">
           Build a routine, enjoy presence.
         </p>
         <div class="block">
           <p>
-            The service is still under development
+            The service is still under development.
           </p>
           <p>
-            Subscribe to be among the firsts to access the app, know about its development 
-            and contribute to the project. Let's make meditation easyer and accessible for everyone. Together.
+            <strong>Subscribe</strong> to be among the firsts to access the app, know about its development 
+            and contribute to the project.
+          </p>
+          <p>
+            Let's make meditation easyer and accessible for everyone. Together.
           </p>
         </div>
         <div class="block">
@@ -49,7 +56,6 @@
 
   .title {
     color: $clear;
-    font-size: clamp(100%, 1rem + 2vw, $size-1);
   }
 
   .hero-bg {
