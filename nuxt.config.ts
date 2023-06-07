61c610e3f33b8ebe5f3718eb65bf3ffc4f4edca5
Daniele Rapisarda <danielerapisarda@Danieles-MacBook-Pro.local>
2023-06-07 19:43:11 +0200

    title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -35,7 +35,7 @@ export default defineNuxtConfig({
       meta: [
         { name: 'robots', content: 'no-index' }
       ],
-      title: 'SereneGrove - your meditation routine'
+      title: 'SereneGrove - Your custom guided meditation routine'
     },
   },
   runtimeConfig: {
