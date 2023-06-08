aa229c0e3cf59cf58cad06a98a0532d1b381ec6f
Daniele Rapisarda <danielerapisarda@Danieles-MacBook-Pro.local>
2023-06-08 11:51:48 +0200

    cache management

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,47 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import svgLoader from 'vite-svg-loader'
 
+interface Device {
+  userAgent: string
+  isDesktop: boolean
+  isIos: boolean
+  isAndroid: boolean
+  isMobile: boolean
+  isMobileOrTablet: boolean
+  isDesktopOrTablet: boolean
+  isTablet: boolean
+  isWindows: boolean
+  isMacOS: boolean
+  isApple: boolean
+  isSafari: boolean
+  isFirefox: boolean
+  isEdge: boolean
+  isChrome: boolean
+  isSamsung: boolean
+  isCrawler: boolean
+}
+
 export default defineNuxtConfig({
   ssr: true,
   modules: [
     '@pinia/nuxt',
     '@pinia-plugin-persistedstate/nuxt',
-    'nuxt-purgecss',
+    ['nuxt-cache-ssr', {
+      // Can be disable per enviroment, like in dev
+      enabled: true,
+      store: {
+        type: 'memory',
+        max: 500,
+        ttl: 1000 * 60 // 1 Minute
+      },
+      pages: [
+        // these are prefixes of pages that need to be cached, for caching homepage use '/'
+        '/',
+        '/about',
+        '/newsletter',
+        '/credits',
+      ],
+    }]
   ],
   piniaPersistedstate: {
     cookieOptions: {
