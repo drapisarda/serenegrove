3a9fc60651412e5fd3f3854a1a24db1f8129df43
Daniele Rapisarda <danielerapisarda@Danieles-MacBook-Pro.local>
2023-06-20 12:26:17 +0200

    start page

---
+++ pages/start.vue
@@ -0,0 +1,16 @@
+<template>
+  <section id="routine" class="section section--clear">
+    <div class="container is-max-widescreen">
+      <h2>
+        Create your routine. Enjoy meditation.
+      </h2>
+      <p>
+        Start by just clicking on the "Add steps to your routine" button.
+      </p>
+      <p></p>
+      <Player />
+      <List />
+    </div>
+  </section>
+
+</template>
