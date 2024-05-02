cc8f029a9eff319e871840856e83d3d70f6ba33d
Daniele Rapisarda <info@danielerapisarda.com>
2024-05-02 21:02:01 +0200

    page visit as e2e test

---
+++ tests/cypress/e2e/pageLoads.cy.js
@@ -0,0 +1,17 @@
+const pages = [
+  {id: 'Home', url: '/'},
+  {id: 'Create', url: '/create/'},
+  {id: 'About', url: '/about/'},
+  {id: 'Why Meditation', url: '/why-meditation/'},
+  {id: 'Privacy', url: '/privacy/'},
+  {id: 'Newsletter', url: '/newsletter/'},
+  {id: 'Credits', url: '/credits/'},
+]
+
+describe('Loads pages', () => {
+  pages.forEach(page => {
+    it(page.id, () => {
+      cy.visit('http://localhost:3000' + page.url)
+    });
+  })
+})
