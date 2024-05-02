6547d4132c6029d1e9d145e94acd6d69ef7aa98a
Daniele Rapisarda <info@danielerapisarda.com>
2024-05-02 20:48:05 +0200

    changed tests folders

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    specPattern: 'tests/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: 'tests/cypress/support/e2e.{js,jsx,ts,tsx}'
   },
 });
