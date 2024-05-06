0040440d23c58974e2b40a98b485b0593ec9bdfe
Daniele Rapisarda <info@danielerapisarda.com>
2024-05-06 16:16:05 +0200

    Test on Modal

---
+++ tests/unit/Modal.spec.ts
@@ -0,0 +1,44 @@
+import Modal from '@/components/Modal.vue'
+import { mount, type VueWrapper } from '@vue/test-utils'
+import { expect } from 'vitest'
+
+describe('Modal component test', async () => {
+  let wrapperWithContent: VueWrapper
+
+  beforeEach(() => {
+    wrapperWithContent = mount(Modal, {
+      slots: {
+        'button-text': '<span>This is button text</span>',
+        'modal-title': '<h1>This is a title</h1>',
+        'modal-content': '<p>This is content</p>',
+      },
+    })
+  })
+
+  test('modal mounts with default options', () => {
+    const wrapper = mount(Modal, {})
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  test('modal mounts with custom content', () => {
+    expect(wrapperWithContent).toMatchSnapshot()
+  })
+
+  test('modal opens and closes', async () => {
+    await wrapperWithContent.find('.modal__button button').trigger('click')
+    expect(wrapperWithContent.vm.modalStatus).toBe(true)
+
+    await wrapperWithContent.find('.modal__close').trigger('click')
+    expect(wrapperWithContent.vm.modalStatus).toBe(false)
+  })
+
+  test('modal interacts with keyboard ', async () => {
+    await wrapperWithContent.find('.modal__button button').trigger('click')
+    await wrapperWithContent.vm.$nextTick()
+    expect(wrapperWithContent.vm.modalStatus).toBe(true)
+
+    await wrapperWithContent.trigger('keyup', { key: 'Escape' })
+    await wrapperWithContent.vm.$nextTick()
+    expect(wrapperWithContent.vm.modalStatus).toBe(false)
+  })
+})
