8e41e4a50b4c8edd0e96cb65cc90d9b75cb2bc3e
Daniele Rapisarda <info@danielerapisarda.com>
2024-05-06 13:34:27 +0200

    loader mount test

--- tests/unit/MountingTests.spec.ts
+++ tests/unit/MountingTests.spec.ts
@@ -2,10 +2,11 @@ import Footer from '@/components/Footer.vue'
 import Hero from '@/components/Hero.vue'
 import Navigation from '@/components/Navigation.vue'
 import Newsletter from '@/components/Newsletter.vue'
+import Loader from '@/components/Loader.vue'
 import { mount } from '@vue/test-utils'
 
 describe('Components mount', async () => {
-  const components = [Footer, Hero, Navigation, Newsletter]
+  const components = [Footer, Hero, Navigation, Newsletter, Loader]
 
   components.forEach((component) => {
     test(`${Object.keys(component)[0]} Mounts`, () => {
