6682f552307bf1d5948f47d348729640418c0a7d
Daniele Rapisarda <info@danielerapisarda.com>
2024-05-22 11:32:16 +0200

    🔧 Vercel Speed insights component

--- app.vue
+++ app.vue
@@ -2,12 +2,14 @@
   <NuxtLoadingIndicator :color="'#FEF8C6'" :height="5" :throttle="5000" />
   <NuxtLayout>
     <NuxtPage />
+    <SpeedInsights />
   </NuxtLayout>
 </template>
 
 <script lang="ts" setup>
 import { useRoutineStore } from '@/store/routine'
 import { useGlobalStore } from '@/store/global'
+import { SpeedInsights } from '@vercel/speed-insights/nuxt'
 
 const { version } = useRoutineStore()
 const { version: globalVersion } = useGlobalStore()
