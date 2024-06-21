d029c5768bba4963a4378033f35a6501508a82ed
Daniele Rapisarda <info@danielerapisarda.com>
2024-06-21 23:44:44 +0200

    :recycle: font swap and mailchip script defer

--- components/Newsletter.vue
+++ components/Newsletter.vue
@@ -83,6 +83,7 @@ useHead({
       children:
         '!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/f668e4d9b5cfc362bf643a0f5/70d223ff4652eb4352dae28cc.js");',
       id: 'mcjs',
+      defer: true,
     },
   ],
 })
